bb9e490671796b70712d63850e11b2aa03deefb4
Joshua Wines <jnwines@gmail.com>
2015-10-23 18:03:21 +0800

    Add complaints resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users, only: [:create] do
+
       collection do
         post 'login'
       end
+
+      resources :complaints, only: [:create], controller: "users/complaints"
+
     end
   end
 
