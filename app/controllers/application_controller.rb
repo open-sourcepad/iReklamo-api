6c1cc6e811c3589fb6de2d7adee62bb3c246f02c
Joshua Wines <jnwines@gmail.com>
2015-10-23 21:22:45 +0800

    Default to json response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
 
+  before_action :set_default_response_format
+
+  protected
+
+  def set_default_response_format
+    request.format = :json
+  end
+  
   def authenticate_token_from_user!
     @user = User.find_by(access_token: request.env["HTTP_AUTHORIZATION"])
     render json: {success: "false", message: "Unauthorized Access"}, status: 401 unless @user
