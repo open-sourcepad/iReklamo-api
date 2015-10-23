910a84497cc390efdf5422c1bdc28a93001beacb
Daryll Santos <daryll.santos@gmail.com>
2015-10-23 17:58:34 +0800

    Add Spring

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
   config.include FactoryGirl::Syntax::Methods
   config.include Requests::JsonHelpers, type: :controller
 
