5c298888cc957be01a3bc586ad866e13afff650e
Joshua Wines <jnwines@gmail.com>
2015-10-23 16:15:05 +0800

    Generate access_token upon user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
 
   validates_presence_of :password, on: :create
 
+  after_create :generate_access_token
+
+  def generate_access_token
+    self.access_token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless User.exists?(access_token: random_token)
+    end
+  end
+
 end
